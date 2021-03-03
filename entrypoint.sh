32656ea317720ebe23cec0c71bfaa70ab60908e5
Courser <GitCourser@users.noreply.github.com>
2021-03-03 16:47:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ echo "$KEY1" > /root/.ssh/id1_rsa
 chmod 600 /root/.ssh/*_rsa
 
 cd /root
-git clone `git@"$SRC".git` repo
+git clone `git@"$1".git` repo
 cd /root/.ssh
 mv id_rsa id0_rsa
 mv id1_rsa id_rsa
 cd /root/repo
-git remote add gitee `git@gitee.com:"$DST1".git`
-git remote add github `git@github.com:"$DST2".git`
+git remote add gitee `git@gitee.com:"$2".git`
+git remote add github `git@github.com:"$3".git`
 git push gitee master
 git push github master
