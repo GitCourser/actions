690bd9199d2ba124a053813d661d7f518272f262
Courser <GitCourser@users.noreply.github.com>
2021-03-03 15:40:47 +0800

    new

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+mkdir -p /root/.ssh
+echo "$KEY0" > /root/.ssh/id_rsa
+echo "$KEY1" > /root/.ssh/id1_rsa
+chmod 600 /root/.ssh/*_rsa
+
+cd /root
+git clone `git@"$SRC".git` repo
+cd /root/.ssh
+mv id_rsa id0_rsa
+mv id1_rsa id_rsa
+cd /root/repo
+git remote add gitee `git@gitee.com:"$DST1".git`
+git remote add github `git@github.com:"$DST2".git`
+git push gitee master
+git push github master
