8585a916f1fd28de2e63a08e645d95b467cfb389
Courser <GitCourser@users.noreply.github.com>
2022-11-30 22:34:18 +0800

    Create wjw.py

---
+++ other/wjw.py
@@ -0,0 +1,55 @@
+# ==============================================================================
+# Author       : Courser
+# Date         : 2022-11-30 21:27:13
+# LastEditTime : 2022-11-30 22:19:32
+# Description  : 本地疫情速报
+# ==============================================================================
+
+import sys
+sys.path.append('..')
+import re
+from playwright.sync_api import sync_playwright
+from common import runtime, WeChat, isCloud
+
+app = '本地疫情速报'
+
+
+def getinfo():
+    with sync_playwright() as p:
+        url = 'https://wjw.hubei.gov.cn/bmdt/dtyw'
+        browser = p.webkit.launch()
+        page = browser.new_page()
+        page.goto(url)
+        resp = page.inner_html('#share')
+        # print(resp)
+        if result := re.search(r'href=".(.+)" title="\d+年\d+月\d+日湖北省新冠肺炎疫情情况"', resp):
+            url = f'{url}{result.group(1)}'
+            # print(url)
+        page.goto(url)
+        # print(page.title())
+        resp = page.inner_text('//*[@id="article-box"]/div[1]/p[1]')
+        page.screenshot(path="example.png")
+        browser.close()
+    return resp
+
+
+@runtime(app)
+def main():
+    txt = getinfo()
+    msg = re.search(r'(\d+月\d+日.+时)，', txt).group(1)
+    msg += '\n\n全省:\n'
+    if foo := re.search(r'全省新增本土(确诊病例\d+例).+新增本土(无症状感染者\d+例)（(武汉市\d+例)，.+(荆州市28例)，', txt):
+        msg += f'{foo.group(1)}\n{foo.group(2)}\n\n市:\n{foo.group(3)}\n{foo.group(4)}'
+    if isCloud:
+        notify = WeChat()
+        notify.send(app, msg)
+    else:
+        print(msg)
+
+
+def run():
+    main()
+
+
+if __name__ == '__main__':
+    run()
