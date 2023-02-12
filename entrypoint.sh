9393b3330be20bf0488fd9e0e6fd9c6a445dc5ae
Courser <GitCourser@users.noreply.github.com>
2023-02-12 14:37:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 mkdir py
 pip install -t py "$1"
 tar -zcvf py.tar.gz py
-mkdir download
-cp py.tar.gz download
+# mkdir download
+# cp py.tar.gz download
 curl -sL https://git.io/file-transfer | sh
-./transfer wet ./download/
+# ./transfer wet ./download/
+./transfer wet py.tar.gz
